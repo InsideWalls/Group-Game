6aa08c602ff84ce687cbf9249a3fdaa70295b142
John <joeshpmother@gmail.com>
2025-03-21 12:58:08 -0400

    Asset polishing part 2 and movement
    Made the cargo crates look better and added guns around the level for the player to pick up. I have only codded the guns to spin so far, so the inventory system will still need to be made.
    
    Added code for the player to move using "WASD" and to let them look around with the mouse.
    
    The level feels less empty now but I am not sure what else I should add as decoration as I'm worrying about overstimulating the player with random goofy objects.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+    public CharacterController controller;
+
+    private Vector3 velocity;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
