bdab34b07e178a47b19479115e5af5cdff7128be
LoganL792 <logan.leichter@gmail.com>
2025-04-16 12:29:11 -0400

    Robot Hacking
    I modified RobotHandling.cs to allow the player to switch between robots. This was implemented in the MainLevel scene.

--- Assets/Scripts/RobotHandling.cs
+++ Assets/Scripts/RobotHandling.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RobotHandling : MonoBehaviour
 {
@@ -7,24 +9,36 @@ public class RobotHandling : MonoBehaviour
     public GameObject playerCam;
     private GameObject robCam;
 
-    private GameObject currentRobot;
     private bool hackable=false;
     private bool hacked=false;
+    
+    private GameObject newRobot;
+    public List<GameObject> robots;
+    private int nextRobotIndex;
+    private int currentRobotIndex;
 
     void Start()
     {
-
+        Debug.Log(pControl.enabled + " | " + playerCam.activeSelf);
+        nextRobotIndex = 0;
+        currentRobotIndex = -1;
     }
 
     void Update()
     {
+        if (robots.Count >0)
+        {
+            robControl = robots[currentRobotIndex].GetComponent<PlayerMovement>();
+            robCam = robots[currentRobotIndex].transform.Find("Main Camera").gameObject;
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            if (hacked)
+            if (robots.Count >0)
             {
                 pControl.enabled=!pControl.enabled;
-                robControl.enabled=!robControl.enabled;
                 playerCam.SetActive(!playerCam.activeSelf);
+                robControl.enabled=!robControl.enabled;                
                 robCam.SetActive(!robCam.activeSelf);
                 Debug.Log("switched");
             }
@@ -34,25 +48,75 @@ public class RobotHandling : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.F) && hackable && !hacked)
+        if (Input.GetKeyDown(KeyCode.F) && newRobot != null)
         {
-            Debug.Log("hacked!");
-            hacked = true;
-            robControl = currentRobot.GetComponent<PlayerMovement>();
-            robCam = currentRobot.transform.Find("Main Camera").gameObject;
+            newRobot.tag = "HackedRobot";
+            newRobot.transform.Find("hackRange").gameObject.SetActive(false);
+            Debug.Log("hacked! robot was set to the " + (nextRobotIndex+1) + " key");
+            if (nextRobotIndex == 0) //if this is the first robot
+            {
+                currentRobotIndex = 0;
+                //robControl = newRobot.GetComponent<PlayerMovement>();
+                //robCam = newRobot.transform.Find("Main Camera").gameObject;
+            }
+
+            robots.Add(newRobot);
+            nextRobotIndex++;
+            newRobot = null;
         }
     }
+
+
+    //*
+    void FixedUpdate()
+    {
+        if (pControl.enabled)
+        {
+            if (Input.GetKey(KeyCode.Alpha1) & robots.Count>0)
+            {
+                currentRobotIndex=0;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha2) & robots.Count > 1)
+            {
+                currentRobotIndex=1;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha3) & robots.Count > 2)
+            {
+                currentRobotIndex=2;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha4) & robots.Count > 3)
+            {
+                currentRobotIndex=3;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha5) & robots.Count > 4)
+            {
+                currentRobotIndex=4;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha6) & robots.Count > 5)
+            {
+                currentRobotIndex=5;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha7) & robots.Count > 6)
+            {
+                currentRobotIndex=6;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+        }
+    }//*/
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "HackRange")
         {
-            hackable = true;
-            if (!hacked)
-            {
-                Debug.Log("Within Range");
-                currentRobot = other.gameObject.transform.parent.gameObject;
-            }
+            Debug.Log("Within Range");
+            //hackable = true;
+            newRobot = other.gameObject.transform.parent.gameObject;
         }
     }
 
@@ -60,12 +124,9 @@ public class RobotHandling : MonoBehaviour
     {
         if (other.gameObject.tag == "HackRange")
         {
-            if (!hacked)
-            {
-                Debug.Log("Left Range");
-                hackable = false;
-                currentRobot = null;
-            }
+            Debug.Log("Left Range");
+            //hackable = false;
+            newRobot = null;
         }
     }
 }
