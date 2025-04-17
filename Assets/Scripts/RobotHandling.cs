1c018ac2ea42d24d9d11bbcf7590ffdcf3050830
John <joeshpmother@gmail.com>
2025-04-17 13:25:53 -0400

    Squashed commit of the following:
    commit bdab34b07e178a47b19479115e5af5cdff7128be
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Wed Apr 16 12:29:11 2025 -0400
    
        Robot Hacking
    
        I modified RobotHandling.cs to allow the player to switch between robots. This was implemented in the MainLevel scene.
    
    commit 2d2a4f9786fa6cdd5e388c884c84fd65e637b067
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Wed Apr 16 09:49:38 2025 -0400
    
        Camera Switch for Robot
    
        Programmed a camera switch for when player switches to using robot
    
    commit baa40cd645eb0485f2230f67a013f678b128f232
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Tue Apr 15 22:43:01 2025 -0400
    
        Robot Functionality
    
        -Added functionality that allowed a robot to be hacked and controlled remotely
    
    commit f92a98494089b7663ab0218ce4a0713b0bd3dcb3
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Tue Apr 15 16:26:54 2025 -0400
    
        Update to minigame progress
    
        The minigame was switched from using a draggable object to simply using arrow keys. This will most likely be easier to implement.
    
    commit 421741d359be0c3c5ec2f0f9a92269a7e7643b0a
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Tue Apr 15 15:28:48 2025 -0400
    
        Hacking minigame
    
        Began work on hacking minigame.
        - Made a real-world UI element w/ a draggable object. Plan to use object as means of navigating minigame
    
    commit 5e4c397c5fa72febaf7762c9abde5c038228a8f4
    Author: renegadeSpiral <61894096+renegadeSpectre-o-d3th@users.noreply.github.com>
    Date:   Tue Apr 1 13:13:11 2025 -0400
    
        Create README.md
    
        Felt like being a bit of a clown. HoNk ;o)
        Don't worry, of course we'll change this to a non-April Fools-y description of the project later. I only did this because we never bothered to make the README before anyway so no harm actually done

---
+++ Assets/Scripts/RobotHandling.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RobotHandling : MonoBehaviour
+{
+    public PlayerMovement pControl;
+    private PlayerMovement robControl;
+    public GameObject playerCam;
+    private GameObject robCam;
+
+    private bool hackable=false;
+    private bool hacked=false;
+    
+    private GameObject newRobot;
+    public List<GameObject> robots;
+    private int nextRobotIndex;
+    private int currentRobotIndex;
+
+    void Start()
+    {
+        Debug.Log(pControl.enabled + " | " + playerCam.activeSelf);
+        nextRobotIndex = 0;
+        currentRobotIndex = -1;
+    }
+
+    void Update()
+    {
+        if (robots.Count >0)
+        {
+            robControl = robots[currentRobotIndex].GetComponent<PlayerMovement>();
+            robCam = robots[currentRobotIndex].transform.Find("Main Camera").gameObject;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (robots.Count >0)
+            {
+                pControl.enabled=!pControl.enabled;
+                playerCam.SetActive(!playerCam.activeSelf);
+                robControl.enabled=!robControl.enabled;                
+                robCam.SetActive(!robCam.activeSelf);
+                Debug.Log("switched");
+            }
+            else
+            {
+                Debug.Log("no robot");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && newRobot != null)
+        {
+            newRobot.tag = "HackedRobot";
+            newRobot.transform.Find("hackRange").gameObject.SetActive(false);
+            Debug.Log("hacked! robot was set to the " + (nextRobotIndex+1) + " key");
+            if (nextRobotIndex == 0) //if this is the first robot
+            {
+                currentRobotIndex = 0;
+                //robControl = newRobot.GetComponent<PlayerMovement>();
+                //robCam = newRobot.transform.Find("Main Camera").gameObject;
+            }
+
+            robots.Add(newRobot);
+            nextRobotIndex++;
+            newRobot = null;
+        }
+    }
+
+
+    //*
+    void FixedUpdate()
+    {
+        if (pControl.enabled)
+        {
+            if (Input.GetKey(KeyCode.Alpha1) & robots.Count>0)
+            {
+                currentRobotIndex=0;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha2) & robots.Count > 1)
+            {
+                currentRobotIndex=1;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha3) & robots.Count > 2)
+            {
+                currentRobotIndex=2;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha4) & robots.Count > 3)
+            {
+                currentRobotIndex=3;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha5) & robots.Count > 4)
+            {
+                currentRobotIndex=4;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha6) & robots.Count > 5)
+            {
+                currentRobotIndex=5;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+            if (Input.GetKey(KeyCode.Alpha7) & robots.Count > 6)
+            {
+                currentRobotIndex=6;
+                Debug.Log("current robot: #" + (currentRobotIndex + 1));
+            }
+        }
+    }//*/
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HackRange")
+        {
+            Debug.Log("Within Range");
+            //hackable = true;
+            newRobot = other.gameObject.transform.parent.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "HackRange")
+        {
+            Debug.Log("Left Range");
+            //hackable = false;
+            newRobot = null;
+        }
+    }
+}
