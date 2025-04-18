b53dd2a11ab709250583736672a0e7f6ddc4719f
LoganL792 <logan.leichter@gmail.com>
2025-04-17 23:55:24 -0400

    Hacking Minigame Update
    I made a playable minigame. It is currently only in LoganTest.

--- Assets/Scripts/MinigameScript.cs
+++ Assets/Scripts/MinigameScript.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MinigameScript : MonoBehaviour
 {
-    public RectTransform playerSquare;
-
     public Canvas canvas;
     private RectTransform canvasRect;
+    public RectTransform playerSquare;
+    public RectTransform endSquare;
+
     private float centerX;
     private float centerY;
     private float centerZ;
+    private float leftX;
+    private float rightX;
+    private float topY;
+    private float bottomY;
+
+    public List<RectTransform> blocks;
+
+    private bool ended=false;
     
     void Start()
     {
@@ -16,52 +27,82 @@ public class MinigameScript : MonoBehaviour
         centerY = canvas.transform.position.y;
         centerZ = canvas.transform.position.z;
 
+        Debug.Log(centerX + ", " + centerY+", "+centerZ);
+        Debug.Log(playerSquare.localPosition.x + ", " + playerSquare.localPosition.y);
+
         canvasRect = canvas.GetComponent<RectTransform>();
-        //Debug.Log(canvasRect.rect.width+" x "+canvasRect.rect.height);
+        //Debug.Log(canvasRect.rect.width+" X "+canvasRect.rect.height);
+        float offset = playerSquare.rect.width/2.0f;
+        leftX = centerX - canvasRect.rect.width/2 + offset;
+        rightX = centerX + canvasRect.rect.width/2-offset;
+        topY = centerY + canvasRect.rect.height/2-offset;
+        bottomY = centerY - canvasRect.rect.height/2 + offset;
+        Debug.Log(leftX + ", " + rightX + ", " + bottomY + ", " + topY);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("right"))
+        if (!ended)
         {
-            playerSquare.position += new Vector3(-5f, 0, 0);
-        }
-        else if (Input.GetKeyDown("left"))
-        {
-            playerSquare.position += new Vector3(5f, 0, 0);
-        }
-        else if(Input.GetKeyDown("up"))
-        {
-            playerSquare.position += new Vector3(0, 5f, 0);
-        }
-        else if (Input.GetKeyDown("down"))
-        {
-            playerSquare.position += new Vector3(0, -5f, 0);
-        }
+            Vector3 lastPos = playerSquare.position;
+            if (Input.GetKeyDown("right"))
+            {
+                playerSquare.position += new Vector3(-5f, 0, 0);
+            }
+            else if (Input.GetKeyDown("left"))
+            {
+                playerSquare.position += new Vector3(5f, 0, 0);
+            }
+            else if (Input.GetKeyDown("up"))
+            {
+                playerSquare.position += new Vector3(0, 5f, 0);
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                playerSquare.position += new Vector3(0, -5f, 0);
+            }
 
-        playerSquare.position = keepInBounds(playerSquare);
+            foreach (RectTransform block in blocks)
+            {
+                if (block.position == playerSquare.position)
+                {
+                    playerSquare.position = lastPos; break;
+                }
+            }
+
+            if (endSquare.position == playerSquare.position)
+            {
+                Debug.Log("you did it :)"); 
+                ended = true;
+                endSquare.position -= new Vector3(0,0,.1f);
+            }
+
+            playerSquare.position = keepInBounds(playerSquare);
+        }
     }
 
+    //*
     Vector3 keepInBounds(RectTransform rect)
     {
-        if (rect.position.x > centerX + 30 - 2.5f)
+        if (rect.position.x > rightX)
         {
-            return new Vector3(centerX + 30 - 2.5f, rect.position.y, centerZ);
+            return new Vector3(rightX, rect.position.y, centerZ);
         }
-        if (rect.position.x < centerX - 30 + 2.5f)
+        if (rect.position.x < leftX)
         {
-            return new Vector3(centerX - 30 + 2.5f, rect.position.y, centerZ);
+            return new Vector3(leftX, rect.position.y, centerZ);
         }
-        if (rect.position.y > centerY + 20 - 2.5f)
+        if (rect.position.y > topY)
         {
-            return new Vector3(rect.position.x, centerY + 20 - 2.5f, centerZ);
+            return new Vector3(rect.position.x, topY, centerZ);
         }
-        if (rect.position.y < centerY - 20 + 2.5f)
+        if (rect.position.y < bottomY)
         {
-            return new Vector3(rect.position.x, centerY - 20 + 2.5f, centerZ);
+            return new Vector3(rect.position.x, bottomY, centerZ);
         }
 
         return rect.position;
     }
+    //*/
 }
