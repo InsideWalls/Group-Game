92c21535ecc444aba3c8965633538912fc4a8ffa
John <joeshpmother@gmail.com>
2025-04-18 15:38:15 -0400

    UI for available/active robots
    Shows ui when a robot has been hacked and puts a circle around them when they have been selected

--- Assets/Scripts/HideUIRobots.cs
+++ Assets/Scripts/HideUIRobots.cs
@@ -9,10 +9,20 @@ public class HideUIRobots : MonoBehaviour
     public GameObject robot5;
     public GameObject robot6;
     public GameObject robot7;
+
+
+    public GameObject circle1;
+    public GameObject circle2;
+    public GameObject circle3;
+    public GameObject circle4;
+    public GameObject circle5;
+    public GameObject circle6;
+    public GameObject circle7;
     void Awake()
     {
 
         hideui();
+        hidecircle();
     }
     public void hideui()
     {
@@ -23,5 +33,15 @@ public class HideUIRobots : MonoBehaviour
         robot5.SetActive(false);
         robot6.SetActive(false);
         robot7.SetActive(false);
+    }
+    public void hidecircle()
+    {
+        circle1.SetActive(false);
+        circle2.SetActive(false);
+        circle3.SetActive(false);
+        circle4.SetActive(false);
+        circle5.SetActive(false);
+        circle6.SetActive(false);
+        circle7.SetActive(false);
     }
 }
