91d021f854f0954d98895b8e312087d1bf63c0f5
John <joeshpmother@gmail.com>
2025-04-21 14:08:55 -0400

    Added health bar
    Added a health bar that lowers whenever the player takes damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,17 +5,54 @@ public class PlayerHealth : MonoBehaviour
     private int currentHealth = 5;
     public float bumpForce = 2f;
 
+    [Header("Hearts")]
+    public GameObject Heart1;
+    public GameObject Heart2;
+    public GameObject Heart3;
+    public GameObject Heart4;
+    public GameObject Heart5;
+
     private void Start()
     {
         Debug.Log("Player health: " + currentHealth);
+        allhearts();
     }
 
+    public void availablehearts()
+    {
+        if (currentHealth == 4)
+        {
+            Heart5.SetActive(false);
+        }
+        if (currentHealth == 3)
+        {
+            Heart4.SetActive(false);
+        }
+        if (currentHealth == 2)
+        {
+            Heart3.SetActive(false);
+        }
+        if (currentHealth == 1)
+        {
+            Heart2.SetActive(false);
+        }
+    }
+    public void allhearts()
+    {
+        Heart1.SetActive(true);
+        Heart2.SetActive(true);
+        Heart3.SetActive(true);
+        Heart4.SetActive(true);
+        Heart5.SetActive(true);
+    }
     public void TakeDamage(int damageAmount, GameObject damageSource)
     {
         int previousHealth = currentHealth;
         currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
 
         Debug.Log("Player health changed from " + previousHealth + " to " + currentHealth);
+
+        availablehearts();
 
         // Push the enemy away
         if (TryGetComponent<Rigidbody>(out var rb))
