2e25c008c0b815bc3d89385f3331c3874228ab05
John <joeshpmother@gmail.com>
2025-04-21 21:32:04 -0400

    Added sound affect to the game over screen buttons
    Made a sound play when you press a button in the death screen
    
    Altered code in the menu scene that does nothing

--- Assets/Scripts/Game Over.cs
+++ Assets/Scripts/Game Over.cs
@@ -1,30 +1,58 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement; // For scene loading
+using UnityEngine.SceneManagement; // To load scenes
+using System.Collections;
+using NUnit.Framework.Constraints;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private Button retryButton; // Assign button
     [SerializeField] private Button menu;
 
+    [Header("Audio")]
+    public AudioClip menunoise;
+    public AudioSource audioSource;
+
+    public bool sound = false;
+
 
     private void Start()
     {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = menunoise;
         checkbutton();
     }
-
     public void checkbutton()
     {
         retryButton.onClick.AddListener(redo); // Add click listener
-        menu.onClick.AddListener(gomenu);
+        menu.onClick.AddListener(gomenu2);
     }
 
 public void redo()
     {
+        menusound();
+        Invoke("rety2", 1.5f);
+        
+    }
+    public void rety2()
+    {
+        menusound();
         SceneManager.LoadScene("Main Level Scene");
     }
 
+    public void gomenu2()
+    {
+        menusound();
+        Invoke("gomenu", 1.5f);
+    }
     public void gomenu() {
         SceneManager.LoadScene("Main Menu Scene");
+    }
+    // Plays noise on button click and then waits for it to finish before the system will read the next bit of code
+    public void menusound()
+    {
+            audioSource.Play();
+            
+        
     }
 }
