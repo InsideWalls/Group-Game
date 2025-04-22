baa60be1b427e9bcf7948aee82dddcae26ecb659
John <joeshpmother@gmail.com>
2025-04-22 12:47:14 -0400

    Squashed commit of the following:
    commit 4253c6e695352a403ffb2a252dc7f571da97ad01
    Author: John <joeshpmother@gmail.com>
    Date:   Tue Apr 22 12:45:15 2025 -0400
    
        Added condition to switch to the win screen
    
        Added a condition to where if the player gets 6 enemy kills, it will switch to the win screen
    
    commit 79997a7ca90e746ae13875857b13c4bcf3f0fd47
    Author: LoganL792 <logan.leichter@gmail.com>
    Date:   Tue Apr 22 08:57:37 2025 -0400
    
        Enemy Death
    
        Made it so that enemies die upon being shot (EnemyChase.cs)
    
    commit cd9b0509bc69224d245037fa57e223c91ae5302c
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 22:01:26 2025 -0400
    
        Added Win screen scene
    
        Added a win scene for when the player wins
    
    commit 9ccc8e89089626532236d1f49f0a271f39e84ecc
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 21:45:27 2025 -0400
    
        Fixed cursor in the death screen
    
        Made the cursor show after transitioning from the level into the death scene
    
    commit 2e25c008c0b815bc3d89385f3331c3874228ab05
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 21:32:04 2025 -0400
    
        Added sound affect to the game over screen buttons
    
        Made a sound play when you press a button in the death screen
    
        Altered code in the menu scene that does nothing
    
    commit ebf5b55cc13e40c52aaf564f6030f51ad81da864
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 15:33:52 2025 -0400
    
        Added Death scene
    
        Added a scene that the player will go to when they run out of health.
    
        Death scene has a retry, quit, and a menu button that are all functional
    
    commit ac0e6941c12a494d0d54f51ef2df4dab0dd3d268
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 14:31:53 2025 -0400
    
        Damage sound affect
    
        Made a sound affect play when the player takes damage
    
    commit 91d021f854f0954d98895b8e312087d1bf63c0f5
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 14:08:55 2025 -0400
    
        Added health bar
    
        Added a health bar that lowers whenever the player takes damage
    
    commit 705a1c4188a571f7f48ce498e86a39dd0cc7360e
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 13:45:40 2025 -0400
    
        Enemy damage and bump
    
        Made it so the enemy will bump away from the player upon collision and lower a health variable by 1
    
    commit cd15d1a2f4412e3cf60433744e55d4462f191bb8
    Author: John <joeshpmother@gmail.com>
    Date:   Mon Apr 21 02:42:41 2025 -0400
    
        Started enemy ai
    
        Made it so an enemy will chase after the player in a straight line
    
    commit 92c21535ecc444aba3c8965633538912fc4a8ffa
    Author: John <joeshpmother@gmail.com>
    Date:   Fri Apr 18 15:38:15 2025 -0400
    
        UI for available/active robots
    
        Shows ui when a robot has been hacked and puts a circle around them when they have been selected
    
    commit e6807606f118532ae435730e7d009f6e85b19095
    Author: John <joeshpmother@gmail.com>
    Date:   Fri Apr 18 14:25:39 2025 -0400
    
        UI images progress
    
        Added images for the ui.
    
        Making this for a backup incase I mess up too much

--- Assets/Scripts/RobotHandling.cs
+++ Assets/Scripts/RobotHandling.cs
@@ -9,24 +9,99 @@ public class RobotHandling : MonoBehaviour
     public GameObject playerCam;
     private GameObject robCam;
 
-    private bool hackable=false;
-    private bool hacked=false;
-    
     private GameObject newRobot;
     public List<GameObject> robots;
     private int nextRobotIndex;
     private int currentRobotIndex;
 
+    public MinigameScript hackGame;
+    public GameObject hackCam;
+
+    public GameObject robot1;
+    public GameObject robot2;
+    public GameObject robot3;
+    public GameObject robot4;
+    public GameObject robot5;
+    public GameObject robot6;
+    public GameObject robot7;
+
+
+    public GameObject circle1;
+    public GameObject circle2;
+    public GameObject circle3;
+    public GameObject circle4;
+    public GameObject circle5;
+    public GameObject circle6;
+    public GameObject circle7;
+
+    public void hideui()
+    {
+        robot1.SetActive(false);
+        robot2.SetActive(false);
+        robot3.SetActive(false);
+        robot4.SetActive(false);
+        robot5.SetActive(false);
+        robot6.SetActive(false);
+        robot7.SetActive(false);
+    }
+    public void hidecircle()
+    {
+        circle1.SetActive(false);
+        circle2.SetActive(false);
+        circle3.SetActive(false);
+        circle4.SetActive(false);
+        circle5.SetActive(false);
+        circle6.SetActive(false);
+        circle7.SetActive(false);
+    }
+
+    public void showui(int nextRobotIndex)
+    {
+        if (nextRobotIndex == 1)
+        {
+            robot1.SetActive(true);
+        }
+        if (nextRobotIndex == 2)
+        {
+            robot2.SetActive(true);
+        }
+        if (nextRobotIndex == 3)
+        {
+            robot3.SetActive(true);
+        }
+        if (nextRobotIndex == 4)
+        {
+            robot4.SetActive(true);
+        }
+        if (nextRobotIndex == 5)
+        {
+            robot5.SetActive(true);
+        }
+        if (nextRobotIndex == 6)
+        {
+            robot6.SetActive(true);
+        }
+        if (nextRobotIndex == 7)
+        {
+            robot7.SetActive(true);
+        }
+    }
+
     void Start()
     {
+        hackGame.enabled = false;
+        hackCam.SetActive(false);
         Debug.Log(pControl.enabled + " | " + playerCam.activeSelf);
         nextRobotIndex = 0;
-        currentRobotIndex = -1;
+        currentRobotIndex = 0;
+
+        hideui();
+        hidecircle();
     }
 
     void Update()
     {
-        if (robots.Count >0)
+        if (robots.Count > 0)
         {
             robControl = robots[currentRobotIndex].GetComponent<PlayerMovement>();
             robCam = robots[currentRobotIndex].transform.Find("Main Camera").gameObject;
@@ -34,11 +109,11 @@ public class RobotHandling : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            if (robots.Count >0)
+            if (robots.Count > 0)
             {
-                pControl.enabled=!pControl.enabled;
+                pControl.enabled = !pControl.enabled;
                 playerCam.SetActive(!playerCam.activeSelf);
-                robControl.enabled=!robControl.enabled;                
+                robControl.enabled = !robControl.enabled;
                 robCam.SetActive(!robCam.activeSelf);
                 Debug.Log("switched");
             }
@@ -48,21 +123,25 @@ public class RobotHandling : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.F) && newRobot != null)
+        if (Input.GetKeyDown(KeyCode.F) && newRobot != null && pControl.enabled)
         {
             newRobot.tag = "HackedRobot";
             newRobot.transform.Find("hackRange").gameObject.SetActive(false);
-            Debug.Log("hacked! robot was set to the " + (nextRobotIndex+1) + " key");
-            if (nextRobotIndex == 0) //if this is the first robot
-            {
-                currentRobotIndex = 0;
-                //robControl = newRobot.GetComponent<PlayerMovement>();
-                //robCam = newRobot.transform.Find("Main Camera").gameObject;
-            }
+            Debug.Log("hacked! robot was set to the " + (nextRobotIndex + 1) + " key");
 
             robots.Add(newRobot);
             nextRobotIndex++;
             newRobot = null;
+
+            showui(nextRobotIndex);
+        }
+
+            if (Input.GetKeyDown(KeyCode.E))
+        {
+            hackGame.enabled = true;
+            hackCam.SetActive(true);
+            pControl.enabled = false;
+            playerCam.SetActive(false);
         }
     }
 
@@ -72,40 +151,54 @@ public class RobotHandling : MonoBehaviour
     {
         if (pControl.enabled)
         {
-            if (Input.GetKey(KeyCode.Alpha1) & robots.Count>0)
+            if (Input.GetKey(KeyCode.Alpha1) & robots.Count > 0)
             {
-                currentRobotIndex=0;
+                currentRobotIndex = 0;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle1.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha2) & robots.Count > 1)
             {
-                currentRobotIndex=1;
+                currentRobotIndex = 1;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle2.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha3) & robots.Count > 2)
             {
-                currentRobotIndex=2;
+                currentRobotIndex = 2;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle3.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha4) & robots.Count > 3)
             {
-                currentRobotIndex=3;
+                currentRobotIndex = 3;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle4.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha5) & robots.Count > 4)
             {
-                currentRobotIndex=4;
+                currentRobotIndex = 4;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle5.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha6) & robots.Count > 5)
             {
-                currentRobotIndex=5;
+                currentRobotIndex = 5;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle6.SetActive(true);
             }
             if (Input.GetKey(KeyCode.Alpha7) & robots.Count > 6)
             {
-                currentRobotIndex=6;
+                currentRobotIndex = 6;
                 Debug.Log("current robot: #" + (currentRobotIndex + 1));
+                hidecircle();
+                circle7.SetActive(true);
             }
         }
     }//*/
