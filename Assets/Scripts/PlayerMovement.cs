6343f3d207ce2c8ed0fc3eed1979c86bb3b66c84
renegadeSpectre-o-d3th <karatakafabo@gmail.com>
2025-04-26 22:16:36 -0400

    Weapon System Created + Player & Ammo Box Prefab
    Created/Modified the weapon system with my shotgun model and several pistol models added to the list of potential weapons. Weapon parameters include:
    Full Auto (boolean) - whether you can hold the mouse down to continuously fire
    Spread - How many degrees bullets can deviate from the direction of the muzzle when firing
    Rounds per Minute - How fast you can fire
    Velocity - How fast the projectiles travel
    Magazine Size - How many times you can fire before reloading
    Projectiles - How many projectiles are launched when firing (mainly so shotguns can exist properly)
    
    In addition to this, I went ahead and made proper prefabs for the Player and Ammo Boxes.
    Some restructuring had to be done to the Player to put it under an empty game object, primarily because of the camera. I even took the time of coloring up the model by painstakingly creating and applying materials from each of the textures the Snake model came with. Why that didn't import with that done already, I wish I knew.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,14 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -9.81f;
     public CharacterController controller;
     public GunSwitch gs; //references inventory script
+    private GunBehavior gb; //reference GunBehavior script
     public Transform gunPos; //reference to where gun will appear relative to player
-    public GameObject[] guns; //set of gun prefabs for reference
+    public GunBehavior[] guns; //set of gun prefabs for reference
+
+    public int ammoCount;
+    public int AmmoInGun => (gs.currentWeapon < gs.weapons.Count)? gs.weapons[gs.currentWeapon].ammoInGun : 0;
+    // Every time you access this variable, it points to the GunBehavior variable.
+    // Ternary Operator: (true or false)? what happens if true : what happens if false
 
     private Vector3 velocity;
 
@@ -34,31 +40,33 @@ public class PlayerMovement : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("triggered");
-        //Debug.Log(other.gameObject.tag);
-        if (other.gameObject.tag == "GunPickup" && this.gameObject.tag == "Player") //is on each floating gun prefab
+        if (other.gameObject.TryGetComponent<HoverAndRotate>(out HoverAndRotate component)) 
+        // is on each floating gun prefab
+        // A check to see if the object has a HoverAndRotate script component.
+        // If there is one, it will enter the statement with that script component assigned to the value, "component" 
         {
-            string str = other.gameObject.name;
-            Debug.Log("name "+str);
-            int index = -1;
-            if (str.Equals("AR_L"))
-                index = 0;
-            else if (str.Equals("AR_M"))
-                index = 1;
-            else if (str.Equals("AR_N"))
-                index = 2;
-            else if (str.Equals("AR_O"))
-                index = 3;
-            else if (str.Equals("AR_P"))
-                index = 4;
-
-            if(index>=0 & index <= 4)
+            if(component.isAmmoPickup) // if the object is an Ammo Pickup, it will work
             {
-                GameObject weapon = Instantiate(guns[index], gunPos.position, gunPos.rotation);
-                weapon.transform.SetParent(gunPos, true);
-                weapon.transform.localScale = new Vector3(10, 10, 10);
-                gs.AddToInventory(weapon);
-                Destroy(other.gameObject);
+                ammoCount += 30;
             }
+            else
+            {
+                string str = other.gameObject.name;
+                Debug.Log($"name {str}"); //Interpollated Stirng. Don't worry about using a bunch of plus signs
+                
+                int index = (int)component.pickupType;
+
+                if (index >= 0 && index <= 10) // & checks both sides no matter what.
+                // && will immediately returning false if the left is false. 
+                // This is called "short-circuiting". More computationally efficient.
+                {
+                    GunBehavior weapon = Instantiate(guns[index], gunPos.position, gunPos.rotation);
+                    weapon.transform.SetParent(gunPos, true);
+                    // weapon.transform.localScale = new Vector3(10, 10, 10);
+                    gs.AddToInventory(weapon);
+                }
+            }
+            Destroy(other.gameObject);
         }
     }
 }
